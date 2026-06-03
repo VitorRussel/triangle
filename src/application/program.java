713db0d2d8b96499d473017e51ec231f36cd8542
Vitor Gabriel Russel <126631073+VitorRussel@users.noreply.github.com>
2026-06-03 08:54:12 -0300

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/VitorRussel/triangle.git git push -u origin main

---
+++ src/application/program.java
@@ -0,0 +1,50 @@
+package application;
+
+import entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+
+        System.out.println("Enter the measures of triangle x: ");
+
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+
+        double areax = x.area();
+
+
+        double areaY = y.area();
+
+        System.out.println("Triangle X area: %.4f%n, areaX");
+
+        System.out.println("Triangle Y area: %.4f%n, areaY");
+
+
+        if (areax > areaY) {
+            System.out.println("Large area: X");
+        } else {
+            System.out.println("Small area: Y");
+        }
+
+        sc.close();
+    }
+}
+
